e4558ad50652dd9a1a5b2067e481012a77f5972d
PutraRHD <putrarahadian22@gmail.com>
2021-11-21 21:17:44 +0700

    commit 2

--- Login/home.php
+++ Login/home.php
@@ -7,7 +7,6 @@
 <html>
 <head>
 	<title>HOME</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
       <div class="container d-flex justify-content-center align-items-center"
@@ -67,7 +66,6 @@
 			    	<?=$_SESSION['name']?>
 			    </h5>
 			    <a href="logout.php" class="btn btn-dark">Logout</a>
-			    <a href="https://www.smkn22jakarta.sch.id/ekskul/" class="btn btn-dark">sokin 22</a>
             </div>
 			</div>
             <!-- END USERS AREA -->
