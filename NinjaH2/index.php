dbed10d19642782f5953824a2febb599a55f8a13
Thiodoris <thiosimarmata@gmail.com>
2021-12-02 23:09:53 +0700

    commit

--- NinjaH2/index.php
+++ NinjaH2/index.php
@@ -9,7 +9,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
-      <div class="container d-flex justify-content-center align-items-center"
+	<div class="container d-flex justify-content-center align-items-center"
       style="min-height: 100vh">
       	<form class="border shadow p-3 rounded card"
       	      action="php/check-login.php" 
@@ -52,7 +52,7 @@
 		</form>
       </div>
 	  <div 
-	  class="photo-cred">
+	  class="photo-cred card">
 			<a class="text text--small" title="Check out their blog." href="img/res6.jpg" target="_blank"></a>
 		</div>
 
